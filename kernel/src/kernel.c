48578f1b1066a6c191cc9cc7a990ca0cf34ef713
carrillofn <fcarrillo@frba.utn.edu.ar>
2022-09-15 14:00:36 -0300

    Kernel: levanta configuraciones y espera a la conexion de una consola

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,5 +1,36 @@
-//
-// Created by alecho on 11/9/22.
-//
-
 #include "kernel.h"
+
+int main(int argc, char* argv[]) {
+
+    validar_argumentos_main(argc);
+
+    char* CONFIG_FILE = argv[1];
+
+    logger = iniciar_logger(LOG_FILE,LOG_NAME);
+    consola_config = iniciar_config(CONFIG_FILE);
+    communication_config = init_connection_config();
+
+    int socket_srv_kernel = levantar_servidor();
+    int socket_cliente = esperar_cliente(socket_srv_kernel,logger);
+    uint32_t modulo = recibir_handshake_inicial(socket_cliente,KERNEL,logger);
+
+}
+
+void validar_argumentos_main(int argumentos){
+    uint32_t expected = 2;
+    if(argumentos < expected){
+		printf(RED"");
+		printf("Cantidad de parametros incorrectos.\n");
+        printf(" > Expected: %d - Given: %d\n",expected,argumentos);
+		printf("   2- Archivo de configuracion\n");
+		printf(RESET"");
+		exit(argumentos);
+	}
+}
+
+int levantar_servidor(){
+    char* ip_kernel = config_get_string_value(communication_config,"IP_KERNEL");
+    char* puerto_kernel = config_get_string_value(communication_config,"PUERTO_KERNEL");
+
+    return iniciar_servidor(ip_kernel,puerto_kernel,logger);
+}
--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,8 +1,16 @@
-//
-// Created by alecho on 11/9/22.
-//
-
 #ifndef TP_2022_2C_CHAMACOS_KERNEL_H
 #define TP_2022_2C_CHAMACOS_KERNEL_H
+
+#include "../../shared/shared.h"
+
+#define LOG_FILE "kernel.log"
+#define LOG_NAME "kernel_log"
+
+t_config* consola_config;
+t_config* communication_config;
+t_log* logger;
+
+//Obtiene el ip y puerto del kernel para iniciar el servidor, devuele el socket
+int levantar_servidor();
 
 #endif //TP_2022_2C_CHAMACOS_KERNEL_H
