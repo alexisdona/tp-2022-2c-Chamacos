fedb43831528e612d1526a5d9feb809cb67788b9
carrillofn <fcarrillo@frba.utn.edu.ar>
2022-09-19 12:25:21 -0300

    Estructura basica del modulo + validacion de argumentos

--- cpu/headers/cpu.h
+++ cpu/headers/cpu.h
@@ -1,8 +1,6 @@
-//
-// Created by alecho on 11/9/22.
-//
-
 #ifndef TP_2022_2C_CHAMACOS_CPU_H
 #define TP_2022_2C_CHAMACOS_CPU_H
+
+#include "../../shared/shared.h"
 
 #endif //TP_2022_2C_CHAMACOS_CPU_H
